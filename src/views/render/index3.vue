9dcfd127685ec2bf2ce43a334affc129813c5dbb
yankuan <kuan.yan@htouhui.com>
2019-08-29 17:01:25 +0800

    render

--- src/views/render/index3.vue
+++ src/views/render/index3.vue
@@ -1,6 +1,25 @@
 <script>
 export default {
-
+  data () {
+    return {
+      list: [
+        { name: 'hah', age: 20 },
+        { name: 'hah2', age: 20 },
+        { name: 'hah3', age: 201 }
+      ]
+    }
+  },
+  render (h) {
+    const list = h(
+      'div',
+      this.list.map((val) => {
+        return h('div', val.name + '--' + val.age)
+      })
+    )
+    return h(
+      'div', [<h2>map循环</h2>, list]
+    )
+  }
 }
 </script>
 
